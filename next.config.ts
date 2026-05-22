fe7b0fcc7f6bb273be2c6b1aec986a86136b86f3
sulistiana18 <suliez.tiana16@gmail.com>
2026-05-22 19:29:41 +0700

    add 2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio";
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? `/${repoName}` : "";
+
 const nextConfig: NextConfig = {
-  devIndicators: false,
+  output: "export",
+  basePath,
+  assetPrefix: basePath,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-
